1d0655ad4ce85fb028c63a9a24dddf9eb1fd3be3
Kevin <kevin.qi@berkeley.edu>
2020-05-12 15:03:20 -0700

    E2E tests for game page

---
+++ cypress/integration/gamepage.spec.js
@@ -0,0 +1,27 @@
+describe("Submit user code", () => {
+  it("visits the hello_world level", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("a").eq(0).click();
+  });
+
+  it("loads the unity game", () => {
+    cy.get(".console").within(() => {
+      cy.get("span", { timeout: 30000 }).contains("Game loaded", {
+        timeout: 30000,
+      });
+    });
+  });
+
+  it("submits user code successfully within 20 seconds", () => {
+    cy.wait(1000);
+
+    cy.contains("Submit").click();
+
+    cy.wait(20000);
+    cy.get(".console").within(() => {
+      cy.get("span", { timeout: 30000 }).contains("Robot Initialized", {
+        timeout: 30000,
+      });
+    });
+  });
+});
--- cypress/integration/init.spec.js
+++ cypress/integration/init.spec.js
@@ -1,9 +1,9 @@
-describe('Cypress', () => {
-  it('is working', () => {
-    expect(true).to.equal(true)
-  })
+describe("Cypress", () => {
+  it("is working", () => {
+    expect(true).to.equal(true);
+  });
 
-  it('visits the app at localhost:3000', () => {
-    cy.visit('http://localhost:3000')	
-  })
-})
+  it("visits the app at localhost:3000", () => {
+    cy.visit("http://localhost:3000");
+  });
+});

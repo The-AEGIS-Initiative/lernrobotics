b7328e40ccaaab9089c72629fd8f8bc7e9c8a167
Kevin <kevin.qi@berkeley.edu>
2020-05-21 20:10:51 -0700

    feat(css): adjust color scheme, refactor level_card into component

---
+++ src/components/level_card.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card, Button } from "antd";
+import { Link } from "react-router-dom";
+
+import "./level_card.css";
+
+const { Meta } = Card;
+
+function LevelCard({ title, description, link }) {
+  return (
+    <div className="level-card">
+      <Card
+        style={{
+          width: "600px",
+          height: "175px",
+          margin: "30px",
+          backgroundColor: "#214775",
+        }}
+      >
+        <Meta title={title} description={description} />
+
+        <Button className="button">
+          <Link to={link}>Start</Link>
+        </Button>
+      </Card>
+    </div>
+  );
+}
+
+export default LevelCard;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,8 +6,12 @@ import { Card, Button } from "antd";
 import LoginRegisterModal from "../components/login_register_modal";
 import GamePage from "./GamePage";
 import TopNavBar from "../components/top_nav_bar";
+import LevelCard from "../components/level_card";
+
 import { AppContext } from "../contexts/AppContext";
 import { Auth } from "aws-amplify";
+
+import "./HomePage.css";
 
 import * as graphqlController from "../graphql/graphql-controller";
 
@@ -26,47 +30,42 @@ function HomePage() {
   const navBarColor = "#3a608d";
 
   return (
-    <div style={{ backgroundColor: "#b2c4d9", height: "100vh" }}>
-      <TopNavBar type="main" backgroundColor={navBarColor} theme="dark" />
-      <nav>
-        <ul
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            paddingTop: "40px",
-          }}
-        >
-          <h1>
-            <b> Tutorial Module #1 - PID Controllers </b>
-          </h1>
-          <Card style={{ width: 600, margin: "30px" }}>
-            <Meta
+    <div className="home-page">
+      <div style={{ backgroundColor: "#ffffff", height: "100vh" }}>
+        <TopNavBar type="main" backgroundColor={navBarColor} theme="dark" />
+        <nav>
+          <ul
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              paddingTop: "40px",
+            }}
+          >
+            <h1 className="module-title">
+              <b> Tutorial Module #1 - PID Controllers </b>
+            </h1>
+            <LevelCard
               title="Hello World"
               description="Get started with our Control Theory tutorial series!"
+              link="/game/hello_world"
             />
-            <Link to="/game/hello_world">Start!</Link>
-          </Card>
 
-          <Card
-            title="Data Abstraction"
-            description="Learn about abstraction, a powerful engineering principle!"
-            style={{ width: 600, margin: "30px" }}
-          >
-            <Link to="/game/data_abstraction">Start!</Link>
-          </Card>
+            <LevelCard
+              title="Data Abstraction"
+              description="Learn about abstraction, a powerful engineering principle!"
+              link="/game/data_abstraction"
+            />
 
-          <Card
-            title="Velocity Control"
-            description="Learn how to implement a proportional feedback controller!"
-            style={{ width: 600, margin: "30px" }}
-          >
-            <Link to="/game/velocity_control">Start!</Link>
-          </Card>
-        </ul>
-      </nav>
-      <LoginRegisterModal />
-      {/**<Button
+            <LevelCard
+              title="Velocity Control"
+              description="Learn how to implement a proportional feedback controller!"
+              link="/game/velocity_control"
+            />
+          </ul>
+        </nav>
+        <LoginRegisterModal />
+        {/**<Button
         onClick={async () => {
           var jsonObject = await graphqlController.getLevel({
             level_name: "hello_world",
@@ -77,6 +76,7 @@ function HomePage() {
         {" "}
         User{" "}
       </Button>*/}
+      </div>
     </div>
   );
 }

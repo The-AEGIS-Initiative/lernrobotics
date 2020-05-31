9795de2bcb88c0e22f005f82ff54b612f95cb523
alex yi <alexyi1234@berkeley.edu>
2020-05-31 01:19:13 -0700

    started level list

---
+++ src/components/level_list.js
@@ -0,0 +1,32 @@
+import { List, Card } from 'antd';
+
+// TODO : onclick
+
+function LevelList({ level_list }) {
+  return (
+    <div className="grid-list">
+      <List
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 2,
+          md: 4,
+          lg: 4,
+          xl: 6,
+          xxl: 3,
+        }}
+        dataSource={level_list}
+        renderItem={item => (
+          <List.Item>
+            <Card title={item[0].level_name}>TODO: Onclick link here</Card>
+          </List.Item>
+        )}
+      >
+      </List>
+    </div>
+  );
+}
+
+export default LevelList;
+
+

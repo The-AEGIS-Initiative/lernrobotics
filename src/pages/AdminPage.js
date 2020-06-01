5d7cf241eafb75a69f6699e9bed0ffb4cdb2822a
alex yi <alexyi1234@berkeley.edu>
2020-05-31 18:55:15 -0700

    major: fixed lists

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import { Link } from "react-router-dom";
 import LevelList from "../components/level_list";
 import * as graphqlController from "../graphql/graphql-controller";
 
 function AdminPage() {
+  const [llist, setLlist] = useState([]);
+
   // async lambda here to pull the level list
-  const level_list = async () => {
-    var jsonObject = await graphqlController.listLevels();
-    console.log(jsonObject);
-  };
+  useEffect(() => {
+    async function get_list() {
+      var jsonObject = await graphqlController.listLevels();
+      console.log(jsonObject);
+      setLlist(jsonObject);
+    }
+    get_list();
+  }, []);
+
   return (
     <div>
       <h1> Welcome to the Admin page! </h1>
       <Link to="/admin/levelbuilder/test_level"> Level Builder </Link>
-      <LevelList>level_list = {level_list}</LevelList>
+      <h2> Editor List: </h2>
+      <LevelList level_list={llist}></LevelList>
     </div>
   );
 }

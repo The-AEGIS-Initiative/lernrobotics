09df50ca516b3f4aa73caa5b4e5c614c8e12a6f6
alex yi <alexyi1234@berkeley.edu>
2020-05-31 17:23:04 -0700

    major: compiles

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LevelList from "../components/level_list";
-
+import * as graphqlController from "../graphql/graphql-controller";
 
 function AdminPage() {
+  // async lambda here to pull the level list
+  const level_list = async () => {
+    var jsonObject = await graphqlController.listLevels();
+    console.log(jsonObject);
+  };
   return (
     <div>
       <h1> Welcome to the Admin page! </h1>
       <Link to="/admin/levelbuilder/test_level"> Level Builder </Link>
-      <LevelList/>
+      <LevelList>level_list = {level_list}</LevelList>
     </div>
   );
 }
-
+//onclick: link to intermediate page w features, then to the level builder page
 export default AdminPage;

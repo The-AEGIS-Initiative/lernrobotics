f46f8d4db0631efbef108b780c47e1ba2b37e1c7
alex yi <alexyi1234@berkeley.edu>
2020-06-04 19:06:29 -0700

    feat: most of level page

--- src/pages/AdminLevelPage.js
+++ src/pages/AdminLevelPage.js
@@ -5,6 +5,8 @@ import * as graphqlController from "../graphql/graphql-controller";
 
 function AdminLevelPage({ levelID }) {
   const [level, setLevel] = useState("");
+
+  const { Search } = Input;
   useEffect(() => {
     async function get_data() {
       var jsonObject = await graphqlController.getLevelByID({ id: levelID });
@@ -15,7 +17,29 @@ function AdminLevelPage({ levelID }) {
     get_data();
   }, []);
   console.log(level);
-  return <h1> Level Management Page: {level.level_name}</h1>;
+
+  // TODO : styles.css for admin pages
+
+  return (
+    <>
+      <h1> Level Management Page: {level.level_name}</h1>
+      <h2> Edit Level Name: </h2>
+      <Search
+        placeholder={"Enter new name for level"}
+        onSearch={(value) =>
+          // TODO : change name
+          //history.push(`/admin/markdowneditor/${value}`)
+          console.log("test")
+        }
+        enterButton="Change Name"
+      />
+      <h2> Level Builder:</h2>
+      <Link to={`/admin/levelbuilder/${level.level_name}`}>
+        {" "}
+        {level.level_name}{" "}
+      </Link>
+    </>
+  );
 }
 
 export default AdminLevelPage;

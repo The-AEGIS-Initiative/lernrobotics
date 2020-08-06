4d00525014d0b0e2fbcb038f4b3d0e5bd852d1e0
Kevin <kevin.qi@berkeley.edu>
2020-08-06 14:47:44 -0700

    chore(lint): Apply ESlint to src/

--- src/index.js
+++ src/index.js
@@ -24,14 +24,14 @@ if (process.env.NODE_ENV === "development") {
   configUpdate.oauth.redirectSignOut = "http://localhost:3000/";
 }
 
-if (amplify_build_env === "dev") {
+if (amplify_build_env == "dev") {
   configUpdate.oauth.redirectSignIn =
     "https://development-robobot.aegisinitiative.io/";
   configUpdate.oauth.redirectSignOut =
     "https://development-robobot.aegisinitiative.io/";
 }
 
-if (amplify_build_env === "staging") {
+if (amplify_build_env == "staging") {
   configUpdate.oauth.redirectSignIn =
     "https://staging-robobot.aegisinitiative.io/";
   configUpdate.oauth.redirectSignOut =
